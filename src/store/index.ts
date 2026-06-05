b6beafce41c468193d80cde95a6c3961977c7416
Aniket Yadav <aniketyadav690@gmail.com>
2026-06-05 20:12:40 +0530

    feat: Conversation slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import uiReducer from "./slices/uiSlice";
 import logsReducer from "./slices/logsSlice";
 import settingsReducer from "./slices/settingsSlice";
+import conversationsReducer from "./slices/conversationsSlice";
 
 export const store = configureStore({
   reducer: {
     ui: uiReducer,
     logs: logsReducer,
     settings: settingsReducer,
+    conversations: conversationsReducer,
   },
 })
 
